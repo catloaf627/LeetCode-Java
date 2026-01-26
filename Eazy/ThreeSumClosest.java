5ee6eac8fc3fdfd26a64410f282c357f6a970b13
Yuqing <wangyuqing.rachel@gmail.com>
2026-01-26 12:25:07 -0800

    Update ThreeSumClosest.java

--- Eazy/ThreeSumClosest.java
+++ Eazy/ThreeSumClosest.java
@@ -9,8 +9,6 @@ public class ThreeSumClosest {
             int r = nums.length-1;
             int minGap = Integer.MAX_VALUE;
             int abs_minGap = Integer.MAX_VALUE;
-            // System.out.printf("⭐️nums[i] = %d , nums[l] = %d, nums[r] = %d\n", nums[i], nums[l], nums[r]);
-            System.out.println("For loop");
             while (l<r) {
                 int gap = nums[i] + nums[l] + nums[r] - target;
                 int abs_gap = Math.abs(gap);
@@ -18,8 +16,6 @@ public class ThreeSumClosest {
                     minGap = gap;
                     abs_minGap = abs_gap;
                 }
-                // System.out.printf("🌸gap = %d, nums[i] = %d , nums[l] = %d, nums[r] = %d\n",gap, nums[i], nums[l], nums[r]);
-                System.out.println("While loop");
                 if (nums[l]+nums[r] < target-nums[i]) {
                     l++;
                 } else {
@@ -28,10 +24,9 @@ public class ThreeSumClosest {
             }
             minGaps[i][0] = minGap;
             minGaps[i][1] = abs_minGap;
-            // System.out.printf("i=%d, the sum which closest to target = %d\n", i, minGaps[i][0]+target);
         }
-        Arrays.sort(minGaps, (a, b) -> Integer.compare(b[1], a[1]));
-        System.out.printf("closest sum is %d, minGap is %d\n", minGaps[0][0] + target, minGaps[0][0]);
+        Arrays.sort(minGaps, (a, b) -> Integer.compare(a[1], b[1]));
+        System.out.println(Arrays.deepToString(minGaps));
         return minGaps[0][0] + target;
     }
     public static void main(String[] args) {
